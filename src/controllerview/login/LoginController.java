a360a91309a63e6d05cad5c404f70c0462d124c5
Simon Sperr <simon.sperr@htl-wels.at>
2020-12-03 11:55:32 +0100

    Added Login functionality

--- src/controllerview/login/LoginController.java
+++ src/controllerview/login/LoginController.java
@@ -6,10 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Login;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -18,6 +22,16 @@ import java.util.ResourceBundle;
  **/
 public class LoginController implements Initializable {
   private Stage stage;
+
+  private ArrayList<Login> validLogins;
+
+  @FXML
+  private TextField username_txt;
+  @FXML
+  private TextField passwd_txt;
+  @FXML
+  private Label incorrect_lbl;
+
   
   public static void show(Stage stage) {
     try {
@@ -28,7 +42,7 @@ public class LoginController implements Initializable {
       LoginController ctrl = fxmlLoader.getController();
       ctrl.stage = stage;
       
-      stage.setTitle("Welcome");
+      stage.setTitle("Login");
       stage.setScene(new Scene(root, 400, 400));
       stage.show();
     }
@@ -40,14 +54,31 @@ public class LoginController implements Initializable {
   
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    validLogins = new ArrayList<Login>();
+    validLogins.add(new Login("Admin", "Admin"));
+    validLogins.add(new Login("username", "password"));
+    validLogins.add(new Login("schueler", "schueler"));
+    validLogins.add(new Login("sperrsim", "password"));
   }
   
   @FXML
-  private void next() {
-    //navigate from welcome screen to main screen
-    System.out.println("Navigation started ...");
-   
-    EightBallController.show(new Stage(),"Hello from Welcome Controller!");
-    stage.close();
+  private void checkLogin() {
+    String username = username_txt.getText();
+    String password = passwd_txt.getText();
+
+    Login login = new Login(username, password);
+
+    if(validLogins.contains(login))
+    {
+      //navigate from welcome screen to main screen
+      System.out.println("Navigation started ...");
+
+      EightBallController.show(new Stage(),"Login successfull!");
+      stage.close();
+    }
+    else
+    {
+      incorrect_lbl.setVisible(true);
+    }
     }
 }
--- src/model/Login.java
+++ src/model/Login.java
@@ -1,8 +1,52 @@
 package model;
 
+import java.util.Objects;
+
 /**
  * @author Simon Sperr
  * @version 2020.1.1, 02.12.2020
  **/
 public class Login {
+    private String username;
+    private String password;
+
+    public Login(String username, String password)
+    {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Login login = (Login)o;
+        return this.username.equals(login.username) &&
+                this.password.equals(login.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    public Login(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
